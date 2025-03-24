ac79da6965e1d01da862831d8b37e202ad9250df
AbelJSanchez <abels1@pm.me>
2025-03-24 15:38:49 -0700

    Added the ability to pull player data from the API

--- extract.py
+++ extract.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv  # pip install python-dotenv
 import json
 import pandas as pd  # pip install pandas
 
+pd.set_option('display.max_columns', None)  # Show all columns
+pd.set_option('display.width', None)  # Use maximum width
+pd.set_option('display.max_rows', None)  # Use maximum width
 
 def extract_teams() -> pd.DataFrame:
     """
@@ -21,15 +24,65 @@ def extract_teams() -> pd.DataFrame:
     json_data = json.loads(data)
     for team in json_data['response']:
         if team.get('nbaFranchise') is True and team.get('allStar') is False:
-            team['conference'] = team['leagues']['standard'].get('conference')
-            team['division'] = team['leagues']['standard'].get('division')
+            team['conference'] = team.get('leagues', {}).get('standard', {}).get('conference')
+            team['division'] = team.get('leagues', {}).get('standard', {}).get('division')
             teams.append(team)
 
     # Create DataFrame containing the selected columns
-    team_columns = ['name', 'nickname', 'code', 'city', 'conference', 'division']
-    data_frame = pd.DataFrame(teams)[team_columns]
-    print(data_frame)
-    return data_frame
+    team_columns = ['id', 'name', 'nickname', 'code', 'city', 'conference', 'division']
+    team_data_frame = pd.DataFrame(teams)[team_columns]
+    print(team_data_frame)
+    return team_data_frame
+
+
+def extract_players() -> pd.DataFrame:
+    """
+    Extracts player data via the API.
+
+    :return: A Pandas DataFrame containing NBA player information with the columns:
+                id, firstname, lastname, team, position, college, birthdate, rookie_year, height_feet,
+                height_inches, weight_pounds, jersey_number
+    """
+
+    # Request team data from the API - First API call
+    connection.request("GET", "/players?team=1&season=2021", headers=headers)
+    response = connection.getresponse()
+    data = response.read()
+    json_data = json.loads(data)
+    player_indexes = {}  #
+
+    # Parse JSON response for player info
+    players = []
+    for i, player in enumerate(json_data['response']):
+        player_indexes[player.get('id')] = i  # Used when adding the player's team in the second API call
+        player['college'] = player.get('college')
+        player['birthdate'] = player['birth'].get('date')
+        player['rookie_year'] = "None" if player['nba'].get('start') == 0 else player['nba'].get('start')
+        player['height_feet'] = "None" if player['height'].get('feets') in (None, "None") else float(player['height'].get('feets'))
+        player['height_inches'] = "None" if player['height'].get('inches') in (None, "None") else float(player['height'].get('inches'))
+        player['weight_pounds'] = "None" if player['weight'].get('pounds') in (None, "None") else float(player['weight'].get('pounds'))
+        player['jersey_number'] = player.get('leagues', {}).get('standard', {}).get('jersey', "None")
+        player['position'] = player.get('leagues', {}).get('standard', {}).get('pos', "None")
+        player['team_id'] = "None"  # Team is updated in the second API call
+        players.append(player)
+
+    # Second API call - Retrieve team info for each player
+    connection.request("GET", "/players/statistics?team=1&season=2021", headers=headers)
+    response = connection.getresponse()
+    data = response.read()
+    json_data = json.loads(data)
+    for player_stat in json_data['response']:
+        player_id = player_stat['player'].get('id')
+
+        if player_id in player_indexes:
+            index = player_indexes[player_id]
+            players[index]['team_id'] = player_stat.get('team', {}).get('id', "None")
+
+    # Create DataFrame containing the selected columns
+    player_columns = ['id', 'firstname', 'lastname', 'team_id', 'position', 'college', 'birthdate', 'rookie_year', 'height_feet', 'height_inches', 'weight_pounds', 'jersey_number']
+    player_data_frame = pd.DataFrame(players)[player_columns]
+    print(player_data_frame)
+    return player_data_frame
 
 
 # Retrieve API and Database credentials from local .env file
@@ -69,6 +122,6 @@ headers = {
     }
 
 teams_df = extract_teams()
-print(teams_df.columns.to_list())
+players_df = extract_players()
 
 connection.close()
